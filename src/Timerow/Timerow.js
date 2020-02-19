755501407bb6a207b523d3412ba14e127bf3db84
Coletta <g.coletta@intersistemi.it>
2020-02-19 15:17:27 +0100

    visual fix

--- src/Timerow/Timerow.js
+++ src/Timerow/Timerow.js
@@ -1,29 +1,16 @@
 import React, { Component } from 'react'
 import { Row, Col, FormControl, Form } from 'react-bootstrap';
-import moment from 'moment'
 
 
 class Timerow extends Component {
     state = {
-        formControl: {
-            amStart: '09:00',
-            amEnd: '13:00',
-            pmStart: '14:00',
-            pmEnd: '18:00'
-        }
-    }
-    handleChange = (e) => {
-        const name = e.target.name;
-        const value = e.target.value;
-        this.setState({
-            formControl: {
-                ...this.state.formControl,
-                [name]: {
-                    ...this.state.formControl[name],
-                    value
-                }
-            }
-        })
+        dayName: this.props.dayName,
+        date: this.props.date,
+        isHolyDay: this.props.isHolyDay,
+        amStart: this.props.amStart,
+        amEnd: this.props.amEnd,
+        pmStart: this.props.pmStart,
+        pmEnd: this.props.pmEnd
     }
     createDayOfWeek(number) {
         let day = ''
@@ -31,56 +18,92 @@ class Timerow extends Component {
             case 0:
                 day = "Domenica";
                 break;
-              case 1:
+            case 1:
                 day = "Lunedi";
                 break;
-              case 2:
-                 day = "Martedi";
+            case 2:
+                day = "Martedi";
                 break;
-              case 3:
+            case 3:
                 day = "Mercoledi";
                 break;
-              case 4:
+            case 4:
                 day = "Giovedi";
                 break;
-              case 5:
+            case 5:
                 day = "Venerdi";
                 break;
-              case 6:
+            case 6:
                 day = "Sabato";
+                break;
+            default:
+                break;
         }
         return day
-
     }
+    handleChange = e => {
+        let newOne = { ...this.state }
+        newOne[e.target.name] = e.target.value
+        this.setState(newOne)
+    }
+
     render() {
         return (
             <Row>
                 <Col>
-                    <p>{this.createDayOfWeek(this.props.day)}</p>
+                    <div style={this.props.dayName === 0 || this.props.dayName === 6 ? { color: 'red' } : { color: 'black' }} >
+                        {this.createDayOfWeek(this.state.dayName)}
+                    </div>
                 </Col>
                 <Col>
                     <Form>
-                        <FormControl type="date" value={this.props.dateValue.format('YYYY-MM-DD')}name="day" className="mr-sm-2" onChange={this.handleChange} />
+                        <FormControl
+                            type="date"
+                            defaultValue={this.state.date}
+                            name="date" 
+                            className="mr-sm-2"
+                            readOnly
+                            style={this.props.dayName === 0 || this.props.dayName === 6 ? { color: 'red' } : { color: 'black' }} />
                     </Form>
                 </Col>
                 <Col>
                     <Form>
-                        <FormControl type="time" defaultValue="09:00" name="amStart" className="mr-sm-2" onChange={this.handleChange} />
+                        <FormControl 
+                            type="time" 
+                            defaultValue={this.props.dayName === 0 || this.props.dayName === 6 ? null : this.state.amStart} 
+                            name="amStart" 
+                            className="mr-sm-2" 
+                            onChange={this.handleChange} />
                     </Form>
                 </Col>
                 <Col>
                     <Form>
-                        <FormControl type="time" defaultValue="13:00" name="amStart" className="mr-sm-2" onChange={this.handleChange} />
+                        <FormControl 
+                            type="time" 
+                            defaultValue={this.props.dayName === 0 || this.props.dayName === 6 ? null : this.state.amEnd} 
+                            name="amEnd" 
+                            className="mr-sm-2" 
+                            onChange={this.handleChange} />
                     </Form>
                 </Col>
                 <Col>
                     <Form>
-                        <FormControl type="time" defaultValue="14:00" name="amStart" className="mr-sm-2" onChange={this.handleChange} />
+                        <FormControl 
+                            type="time" 
+                            defaultValue={this.props.dayName === 0 || this.props.dayName === 6 ? null : this.state.pmStart} 
+                            name="pmStart" 
+                            className="mr-sm-2" 
+                            onChange={this.handleChange} />
                     </Form>
                 </Col>
                 <Col>
                     <Form>
-                        <FormControl type="time" defaultValue="18:00" name="amStart" className="mr-sm-2" onChange={this.handleChange} />
+                        <FormControl 
+                            type="time" 
+                            defaultValue={this.props.dayName === 0 || this.props.dayName === 6 ? null : this.state.pmEnd} 
+                            name="pmEnd" 
+                            className="mr-sm-2" 
+                            onChange={this.handleChange} />
                     </Form>
                 </Col>
             </Row>
--- src/Timesheet/Timesheet.js
+++ src/Timesheet/Timesheet.js
@@ -4,21 +4,38 @@ import moment from 'moment'
 import Timerow from '../Timerow/Timerow';
 
 class Timesheet extends Component {
-    getDays(listOfDays) {
+    state = {
+        days: []
+    }
+    componentDidMount() {
+        console.log('Initializing Component ' + this.constructor.name)
+        this.getDays()
+    }
+    getDays() {
         let startdate = moment().format('YYYY-MM-01');
         const numberOfDays = moment().daysInMonth();
+        let joined = [...this.state.days]
         for (let i = 0; i < numberOfDays; i++) {
             let new_date = moment(startdate, "YYYY-MM-DD").add(i , 'days');
             let dayOfWeek = new_date.weekday()
-            listOfDays.push(
-                <Timerow day={dayOfWeek}dateValue={new_date} key={i}/>
-            )
-            console.log(dayOfWeek)
+            let dayFlag = false
+            if (dayOfWeek === 0 || dayOfWeek === 6) {
+                dayFlag = true
+            }
+            joined.push({
+                    index: i,
+                    dayName: dayOfWeek,
+                    date: new_date.format('YYYY-MM-DD'),
+                    isHolyDay: dayFlag,
+                    amStart: '09:00',
+                    amEnd: '13:00',
+                    pmStart: '14:00',
+                    pmEnd: '18:00' 
+            })
         }
+        this.setState({days: joined})
     }
     render() {
-        const listOfActiveDay = [];
-        this.getDays(listOfActiveDay)
         return (
             <Container>
                 <Row>
@@ -41,7 +58,21 @@ class Timesheet extends Component {
                         <h5>PM End</h5>
                     </Col>
                 </Row>
-                {listOfActiveDay}
+                {this.state.days.map( day => {
+                        return (
+                            <Timerow
+                                key={day.index}
+                                dayName={day.dayName}
+                                date={day.date}
+                                isHolyDay={day.isHolyDay}
+                                amStart={day.amStart}
+                                amEnd={day.amEnd}
+                                pmStart={day.pmStart}
+                                pmEnd={day.pmEnd}
+                                value={day}
+                            />
+                        )
+                    })}
             </Container>
         );
     }
